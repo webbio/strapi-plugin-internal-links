533db25fc7451f0c999a60b4e4fa8815dcbc7be7
Maikel Mast <maikel@webbio.nl>
2023-01-26 12:17:18 +0100

    chore: delete ckeditor

--- admin/src/components/ckeditor/ckeditor-plugins/strapi-internal-link/index.js
+++
@@ -1,4 +0,0 @@
-export { default } from './link';
-export { default as LinkEditing } from './link-editing';
-export { default as LinkUI } from './link-ui';
-export { default as AutoLink } from './auto-link';
