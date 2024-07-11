bca0b3dda10222b6c7f132758b1aa420d6f19937
Tom Klein <tom@webbio.nl>
2024-07-11 13:59:11 +0200

    fix: no /

--- src/plugins/strapi-plugin-internal-links/admin/src/components/form/index.tsx
+++ src/plugins/strapi-plugin-internal-links/admin/src/components/form/index.tsx
@@ -91,7 +91,7 @@ const InternalLinkForm = ({
 			externalApiValue: props ? props.value : '',
 			externalApiLabel: props ? props.label : '',
 			startPointReference: props.startPointReference || '',
-			url: `/?startpunt=${props.startPointReference}`
+			url: `?startpunt=${props.startPointReference}`
 		}));
 	};
 
@@ -99,7 +99,7 @@ const InternalLinkForm = ({
 		setLink((previousValue) => ({
 			...previousValue,
 			decisionTreeId: id,
-			url: `/?startpunt=${startPointReference}` // ?startpunt=$2b$10$8cNm4eyO1AquoLmlDacnRO
+			url: `?startpunt=${startPointReference}` // ?startpunt=$2b$10$8cNm4eyO1AquoLmlDacnRO
 		}));
 	};
 
